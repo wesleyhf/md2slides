6acd0171c803c85e7ffa84006110286d219bcb57
Wesley Francisco <wesleyhf.contato@gmail.com>
2019-07-14 02:15:42 -0300

    settings/visual improvements

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,57 @@
 <template>
     <div id="app" class="font-sans">
-        <div class="slide-wrapper">
-            <slide :slides="slides"></slide>
+        <div class="w-screen h-screen">
+            <slide
+                :title="title"
+                :slides="slides"
+            ></slide>
         </div>
 
-        <button type="button" @click="toggleSideBar()">toggleSideBar</button>
-
-        <div
-            class="fixed inset-y-0 right-0 w-1/4 bg-blue-400 sidebar"
-            :class="sideBarClasses"
+        <aside
+            class="fixed inset-y-0 right-0 w-1/4 p-4 bg-gray-100 border-l border-gray-200 sidebar"
+            :class="{ active: showSettings }"
         >
-            <textarea v-model="markdown" rows="10"></textarea>
-        </div>
+            <div>
+                <h1 class="text-3xl mb-4">Settings</h1>
+
+                <div class="mb-4">
+                    <label class="block mb-2" for="title">Title</label>
+                    <input
+                        id="title"
+                        type="text"
+                        placeholder="Title"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        v-model="title"
+                    >
+                </div>
+
+                <div class="mb-4">
+                    <label class="block mb-2" for="markdown">Markdown</label>
+                    <textarea
+                        id="markdown"
+                        type="text"
+                        placeholder="Markdown"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        v-model="markdown"
+                        rows="10"
+                    ></textarea>
+                </div>
+            </div>
+
+            <button
+                type="button"
+                class="bg-gray-100 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center absolute bottom-0 left-0 sidebar-btn"
+                :class="{ active: showSettings }"
+                @click="toggleSettings()"
+            >
+                <svg class="fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><polygon points="3.828 9 9.899 2.929 8.485 1.515 0 10 .707 10.707 8.485 18.485 9.899 17.071 3.828 11 20 11 20 9 3.828 9"/></svg>
+            </button>
+        </aside>
     </div>
 </template>
 
 <script>
 import Slide from './components/Slide.vue'
-
-const storageKey = 'markdown';
 
 export default {
     name: 'app',
@@ -28,8 +61,9 @@ export default {
     },
 
     data: () => ({
+        title: '',
         markdown: '',
-        showSideBar: false,
+        showSettings: false,
     }),
 
     computed: {
@@ -39,32 +73,40 @@ export default {
                 .split('@end')
                 .map(slide => slide.trim());
         },
-
-        sideBarClasses() {
-            return {
-                'active': this.showSideBar,
-            };
-        },
     },
 
     methods: {
         setLocalStorage() {
-            localStorage.setItem(
-                storageKey,
-                JSON.stringify(this.markdown)
-            );
+            const settings = {
+                title: this.title,
+                markdown: this.markdown,
+            };
+
+            localStorage.setItem('settings', JSON.stringify(settings));
         },
 
         getLocalStorage() {
-            const encodedMarkdown = localStorage.getItem(storageKey);
+            const settings = JSON.parse(
+                localStorage.getItem('settings')
+            );
 
-            if (encodedMarkdown) {
-                this.markdown = JSON.parse(encodedMarkdown);
+            console.log(settings);
+
+            if (! settings) {
+                return;
+            }
+
+            if (settings.markdown) {
+                this.markdown = settings.markdown;
+            }
+
+            if (settings.title) {
+                this.title = settings.title;
             }
         },
 
-        toggleSideBar() {
-            this.showSideBar = !this.showSideBar;
+        toggleSettings() {
+            this.showSettings = !this.showSettings;
         },
     },
 
@@ -79,18 +121,26 @@ export default {
 @tailwind components;
 @tailwind utilities;
 
-.slide-wrapper {
-    width: 960px;
-    height: 540px;
-    margin: 0 auto;
-}
-
 .sidebar {
     transform: translateX(100%);
     transition: transform .5s ease;
 
     &.active {
         transform: translateX(0);
+    }
+}
+
+.sidebar-btn {
+    transform: translateX(-100%);
+
+    svg {
+        transition: transform .5s ease;
+    }
+
+    &.active {
+        svg {
+            transform: rotate(180deg);
+        }
     }
 }
 </style>
--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="w-full h-full flex flex-col">
         <header class="px-8 py-4 flex justify-between border-b border-gray-200">
-            <h1>presentation</h1>
+            <h1 v-text="title"></h1>
 
             <div>
-                <div v-text="pageCount"></div>
-                <button type="button" @click="prev()">prev</button>
-                <button type="button" @click="next()">next</button>
+                <button type="button" @click="prev()">
+                    <svg class="fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M7.05 9.293L6.343 10 12 15.657l1.414-1.414L9.172 10l4.242-4.243L12 4.343z"/></svg>
+                </button>
+
+                <span v-text="pageCount"></span>
+
+                <button type="button" @click="next()">
+                    <svg class="fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
+                </button>
             </div>
         </header>
 
@@ -35,6 +41,11 @@ import marked from 'marked';
 
 export default {
     props: {
+        title: {
+            type: String,
+            required: true,
+        },
+
         slides: {
             type: Array,
             required: true,
@@ -70,7 +81,7 @@ export default {
 
         progressWidth() {
             const width = (this.localCurrent * 100) / (this.slides.length-1);
-            
+
             return {
                 width: `${width}%`,
             };
@@ -83,16 +94,44 @@ export default {
         },
 
         prev() {
-            this.localCurrent = this.localCurrent-1;
+            if (this.localCurrent > 0) {
+                this.localCurrent = this.localCurrent - 1;
+            }
         },
 
         next() {
-            this.localCurrent = this.localCurrent+1;
+            const limit = this.slides.length - 1;
+
+            if (this.localCurrent < limit) {
+                this.localCurrent = this.localCurrent + 1;
+            }
+        },
+
+        onKeyUp(event) {
+            switch (event.keyCode) {
+                case 37: // left
+                    this.prev();
+                    break;
+
+                case 39: // right
+                    this.next();
+                    break;
+
+                case 32: // space
+                    this.next();
+                    break;
+            }
         },
     },
 
     mounted() {
         this.localCurrent = this.current;
+
+        document.addEventListener('keyup', this.onKeyUp);
+    },
+
+    destroyed() {
+        document.removeEventListener('keyup', this.onKeyUp);
     },
 }
 </script>
